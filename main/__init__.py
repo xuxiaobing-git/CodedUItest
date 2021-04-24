d643fbeaf7ab35b7f7e9d57eb39bf2c00583626e
xuxiaobing-git <xu19890913@163.com>
2021-04-24 21:40:28 +0800

    开始我们的UI自动化

---
+++ main/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/24 21:29
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ main/helloWord.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/4/24 21:37
+# @File    : helloWord.py
+# @Software: PyCharm
+print("hellWord")
